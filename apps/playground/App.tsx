243c376edd83d1a1c371e44ed073a27117039f26
a2shn <209373144+a2shn@users.noreply.github.com>
2026-04-04 11:31:57 +0200

    feat(playground): enable more playbook examples for testing

--- apps/playground/App.tsx
+++ apps/playground/App.tsx
@@ -2,19 +2,19 @@ import { PlaybookApp, type PlaybookConfig } from '@base-ui-rn/playbook';
 import * as React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-// import { SwitchPlaybook } from '@/switch.playbook';
+import { SwitchPlaybook } from '@/switch.playbook';
 import { AccordionPlaybook } from './src/accordion.playbook';
 import { AvatarPlaybook } from './src/avatar.playbook';
 import { ButtonPlaybook } from './src/button.playbook';
-// import { CollapsiblePlaybook } from './src/collapsible.playbook';
-// import { InputPlaybook } from './src/input.playbook';
+import { CollapsiblePlaybook } from './src/collapsible.playbook';
+import { InputPlaybook } from './src/input.playbook';
 import { MeterPlaybook } from './src/meter.playbook';
 import { ProgressPlaybook } from './src/progress.playbook';
 // import { RadioPlaybook } from './src/radio.playbook';
 // import { ScrollAreaPlaybook } from './src/scroll-area.playbook';
 import { SeparatorPlaybook } from './src/separator.playbook';
 // import { SliderPlaybook } from './src/slider.playbook';
-// import { TabsPlaybook } from './src/tabs.playbook';
+import { TabsPlaybook } from './src/tabs.playbook';
 import { ToggleGroupPlaybook } from './src/toggle-group.playbook';
 import { TogglePlaybook } from './src/toggle.playbook';
 
@@ -34,16 +34,16 @@ const REGISTRY: PlaybookConfig = {
     testID: 'button',
     title: 'Button',
   },
-  // Collapsible: {
-  //   component: CollapsiblePlaybook,
-  //   testID: 'collapsible',
-  //   title: 'Collapsible',
-  // },
-  // Input: {
-  //   component: InputPlaybook,
-  //   testID: 'input',
-  //   title: 'Input',
-  // },
+  Collapsible: {
+    component: CollapsiblePlaybook,
+    testID: 'collapsible',
+    title: 'Collapsible',
+  },
+  Input: {
+    component: InputPlaybook,
+    testID: 'input',
+    title: 'Input',
+  },
   Meter: {
     component: MeterPlaybook,
     testID: 'meter',
@@ -75,16 +75,16 @@ const REGISTRY: PlaybookConfig = {
   //   testID: 'slider',
   //   title: 'Slider',
   // },
-  // Switch: {
-  //   component: SwitchPlaybook,
-  //   testID: 'switch',
-  //   title: 'switch',
-  // },
-  // Tabs: {
-  //   component: TabsPlaybook,
-  //   testID: 'tabs',
-  //   title: 'Tabs',
-  // },
+  Switch: {
+    component: SwitchPlaybook,
+    testID: 'switch',
+    title: 'switch',
+  },
+  Tabs: {
+    component: TabsPlaybook,
+    testID: 'tabs',
+    title: 'Tabs',
+  },
   Toggle: {
     component: TogglePlaybook,
     testID: 'toggle',
